d7239ab0f361af442d00dbea80a9b09ba00fc5c9
Kendall Bailey <kbailey@trinity.edu>
2013-02-05 11:09:56 -0600

    First Commit

---
+++ GitHubTestProject/test1.c
@@ -0,0 +1,26 @@
+/*
+ * test1.c
+ *
+ *  Created on: Dec 12, 2012
+ *      Author: kbailey
+ */
+
+
+int main (void){
+	Test1(1);
+	Test1(4);
+
+	Test2("qwer");
+}
+
+void Test1(int num){
+	for(int i = 0; i < num; i++){
+		printf("%d ", i);
+
+	}
+	printf("\n");
+}
+
+void Test2(char *str){
+	printf("%s", str);
+}
---
+++ GitHubTestProject/test1.h
@@ -0,0 +1,18 @@
+/*
+ * test1.h
+ *
+ *  Created on: Dec 12, 2012
+ *      Author: kbailey
+ */
+
+#ifndef TEST1_H_
+#define TEST1_H_
+#include <stdio.h>
+
+
+int main ();
+Test1(int num);
+Test2(char *);
+
+
+#endif /* TEST1_H_ */
